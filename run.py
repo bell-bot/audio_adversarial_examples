6a921c97aed16ae8b5a3d5b7f749c8477ed8bf80
Bella <annabel.jakob@hotmail.de>
2022-03-10 18:19:53 +0000

    Changed run.py to use datasets.py

--- run.py
+++ run.py
@@ -1,18 +1,24 @@
 from generate_target_sents import get_inaugural_sentences
 import attack
 import os
+import datasets
+from datasets import CTRLF_DatasetWrapper
 
 def main():
     # Load the inaugural sentences as targets
     #paras = get_inaugural_sentences()
 
-    # Get the wav files
-    filenames = ["/audio_adversarial_examples/data/" + filename for filename in os.listdir("/audio_adversarial_examples/data/")]
-    for i in range(len(filenames)):
-        filename = filenames[i]
+    x = CTRLF_DatasetWrapper(path_to_labels_csv = "/audio_adversarial_examples/Data", path_to_TED="/audio_adversarial_examples/Data")
+
+    num_files = 100
+
+    for i in range(num_files):
+        sample = x.get(i)
+        out_filename = f"{i}_adversarial.wav"
+        sample_waveform = sample[]
         #para = paras[i]
-        out = [filename[:-4]+"_adversarial.wav"]
-        attack.main(inp = [filename], target = "this is a test please work or else", out = out, iterations = 1000)
+        #out = [filename[:-4]+"_adversarial.wav"]
+        #attack.main(inp = [filename], target = "this is a test please work or else", out = out, iterations = 1000)
 
 
 main()
