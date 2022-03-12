7ebb4c0bb6c2c2c7d6eebd08d9be9c7a6bc14edd
Bella <57637399+bell-bot@users.noreply.github.com>
2022-03-12 22:01:26 +0000

    convert audio array to wav file

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import attack
 import os
 import datasets
 from datasets import CTRLF_DatasetWrapper
+import wave
 
 def main():
     # Load the inaugural sentences as targets
@@ -13,10 +14,14 @@ def main():
     num_files = 1
 
     for i in range(num_files):
-        sample = x.get(i)
+        ted_results, mswc_result, label_dict = x.get(i)
+        sample_waveform = ted_results.get("waveform")
+        filename = "temp_file"
+        wav_file = wave.open(filename, "wb")
+        wav_file.writeframes(sample_waveform)
         out_filename = str(i)+"_adversarial.wav"
         #para = paras[i]
-        attack.main(inp = [sample], target = "this is a test please work or else", out = out_filename, iterations = 100)
+        attack.main(inp = [wav_file], target = "this is a test please work or else", out = out_filename, iterations = 100)
 
 
 main()
