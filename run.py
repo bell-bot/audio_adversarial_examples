f77874e2c1c1a5c622402ce17c7d538e7b650850
Bella <57637399+bell-bot@users.noreply.github.com>
2022-03-12 22:08:44 +0000

    omo

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def main():
         filename = "temp_file"
         wav_file = wave.open(filename, "wb")
         wav_file.setnchannels(1)
-        wav_file.writeframes(sample_waveform)
+        wav_file.writeframesraw(sample_waveform)
         out_filename = str(i)+"_adversarial.wav"
         #para = paras[i]
         attack.main(inp = [wav_file], target = "this is a test please work or else", out = out_filename, iterations = 100)
