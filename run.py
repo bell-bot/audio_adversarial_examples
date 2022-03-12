ee6e025f58b529fedcfe424289370615e1b3b6bc
Bella <57637399+bell-bot@users.noreply.github.com>
2022-03-12 22:26:03 +0000

    using soundfile now

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import attack
 import os
 import datasets
 from datasets import CTRLF_DatasetWrapper
-import scipy.io.wavfile as swav
+import soundfile as sf
 
 def main():
     # Load the inaugural sentences as targets
@@ -16,9 +16,10 @@ def main():
     for i in range(num_files):
         ted_results, mswc_result, label_dict = x.get(i)
         sample_waveform = ted_results.get("waveform")
+        sample_rate = ted_results.get("sample_rate")
         print(sample_waveform)
         filename = "temp_file"
-        wav_file = swav.write(filename, len(sample_waveform), sample_waveform)
+        wav_file = sf.write(filename, sample_waveform, sample_rate)
         out_filename = str(i)+"_adversarial.wav"
         #para = paras[i]
         attack.main(inp = [wav_file], target = "this is a test please work or else", out = out_filename, iterations = 100)
