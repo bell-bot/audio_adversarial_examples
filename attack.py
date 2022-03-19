cae7a1f1d9adcb1bd6ca6cbe3503c357c4a0ee68
Bella <57637399+bell-bot@users.noreply.github.com>
2022-03-19 17:51:34 +0000

    close session after use

--- attack.py
+++ attack.py
@@ -361,4 +361,4 @@ def main(inp = [], target = None, out = "", iterations = None, restore_path = "d
                                            -2**15, 2**15-1),dtype=np.int16))
                 print("Final distortion", np.max(np.abs(deltas[i][:lengths[i]]-audios[i][:lengths[i]])))
               
-        tf.get_variable_scope().reuse_variables()
+        sess.close()
